a738bf6785a1b747da00db906417e6a2215be033
Pedro Correa <pedro8correa@gmail.com>
2019-09-29 15:17:33 -0300

    Fixed player round

--- main.py
+++ main.py
@@ -3,20 +3,17 @@ from models.Player import Player
 
 if __name__ == '__main__':
     board = Board()
-    turno = 1;
+    player1 = Player('y')
+    player2 = Player('x')
 
     while board.x_keys_qtd > 0 and board.y_keys_qtd > 0:
-        board.print_board()
-        
-        if turno % 2 == 0:
-            player = Player('2')
-            print("Player 2 ")
-            
-        else:
-            player = Player('1')
-            print("Player 1 ")
+        player = player2 if board.round % 2 == 0 else player1
 
-        [piece_pos_x, piece_pos_y, pos_x, pos_y] = map(int, input(
-            "Insert position of piece as: piece_pos_x, piece_pos_y, pos_to_x, pos_to_y\n").split(" "))
-        if board.walk_keys_to_pos(piece_pos_x, piece_pos_y, pos_x, pos_y, player.format) is True:
-            turno = turno + 1
+        board.print_info(player)
+
+        [piece_pos_x, piece_pos_y, pos_x, pos_y] = map(
+            int,
+            input("Insert position of piece as: piece_pos_x, piece_pos_y, pos_to_x, pos_to_y\n").split(" ")
+        )
+
+        board.walk_keys_to_pos(piece_pos_x, piece_pos_y, pos_x, pos_y, player)
