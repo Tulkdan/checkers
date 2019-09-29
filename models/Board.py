3613b4c3d658d28e4dd9c03120581d8a5925c15e
Gazola2 <ygoro1@hotmail.com>
2019-09-29 15:55:56 -0300

    Player (#3)
    * pecas do player
    
    * pecas do player 2
    
    * mudancas no turno e nos players
    
    * tentaiva de melhoria
    
    * Fixed player round

--- models/Board.py
+++ models/Board.py
@@ -1,5 +1,9 @@
 from models.Piece import Piece
 
+def message_error(message):
+    print(f"Wait a minute, {message}")
+    print("Try again")
+
 class Board:
     def __init__(self):
         self.BOARD = [
@@ -14,8 +18,19 @@ class Board:
         ]
         self.x_keys_qtd = 12
         self.y_keys_qtd = 12
+        self.round = 1
+
+    def print_info(self, player):
+        self.print_board()
+        self.print_round()
+        print(f'--------- PLAYER {player.format} TURN ---------')
+
+
+    def print_round(self):
+        print(f'------------ ROUND {self.round} ------------')
 
     def print_board(self):
+        output = ''
         LINE = '|---' * 8
         for row in self.BOARD:
             string = '| '
@@ -25,42 +40,46 @@ class Board:
             print(string)
         print(LINE + '|')
 
-    def walk_keys_to_pos(self, piece_x, piece_y, pos_x, pos_y):
+    def walk_keys_to_pos(self, piece_x, piece_y, pos_x, pos_y, player):
+        is_piece_valid_for_player = self.check_player_piece(player, piece_x, piece_y)
         valid_piece = not self.check_place_is_empty(piece_x, piece_y)
         valid_position = self.check_walk_position_is_valid(piece_x, piece_y, pos_x, pos_y)
         is_empty = self.check_place_is_empty(pos_x, pos_y)
         is_position_available = [pos_x, pos_y] in self.positions_available_to_walk(piece_x, piece_y)
 
+        if not is_piece_valid_for_player:
+            message_error("that's not your piece")
+            return
+
         if not valid_piece:
-            print("There's no piece in the given position")
-            print("Try again")
+            message_error("there's no piece in the given position")
             return
 
         if not valid_position:
-            print("Invalid position to walk")
-            print("Try again")
+            message_error("that's an invalid position to walk")
             return
 
         if not is_empty:
-            print("There's a piece in the location that you want to move")
-            print("Try again")
+            message_error("there's a piece in the location that you want to move")
             return
 
         if not is_position_available:
-            print("Invalid position to walk")
-            print("try again")
+            message_error("this position is invalid to walk")
             return
 
-        # TODO: validate if has eaten a piece
         all_squares = self.get_all_pieces_in_diagonal(piece_x, piece_y, pos_x, pos_y)
         if len(all_squares) and not self.remove_piece_if_had_jumped_houses(all_squares):
-            print("Couldn't remove piece")
-            print("Try again")
+            message_error("we couldn't remove the piece")
             return
 
         key = self.BOARD[piece_y][piece_x]
         self.BOARD[piece_y][piece_x] = ' '
         self.BOARD[pos_y][pos_x] = Piece(key.format)
+        self.round += 1
+
+    def check_player_piece(self, player, pos_x, pos_y):
+        piece = self.get_piece_at_position(pos_x, pos_y)
+        return player.format == piece.format
 
     def remove_piece_if_had_jumped_houses(self, houses):
         valid = []
@@ -72,15 +91,15 @@ class Board:
 
         for house in houses:
             if not self.check_place_is_empty(house[0], house[1]):
-                if self.get_piece_at_position(house[0], house[1]) == 'x':
+                if self.get_piece_at_position(house[0], house[1]).format == 'x':
                     print("Removed a X piece")
                     self.x_keys_qtd -= 1
                 else:
                     print("Removed a Y piece")
                     self.y_keys_qtd -= 1
                 self.BOARD[house[1]][house[0]] = ' '
+        print(self.x_keys_qtd, self.y_keys_qtd)
         return True
-
 
     def get_all_pieces_in_diagonal(self, origin_x, origin_y, pos_x, pos_y):
         diff_x = pos_x - origin_x
@@ -107,19 +126,20 @@ class Board:
         result.append([origin_x, origin_y])
         return result
 
-    def get_position_available_piece_not_queen(self, pos_x, pos_y, piece):
+    def get_position_available_piece_not_queen(self, pos_x, pos_y, piece, fn):
+        new_pos_x, new_pos_y = fn(pos_x, pos_y)
         if self.check_place_is_empty(pos_x, pos_y):
             return [pos_x, pos_y]
-        elif self.check_place_is_empty(pos_x + 1, pos_y + 1) and self.get_piece_at_position(pos_x, pos_y) != piece.format:
-            return [pos_x + 1, pos_y + 1]
-
+        elif self.check_place_is_empty(new_pos_x, new_pos_y) and self.get_piece_at_position(pos_x, pos_y) != piece.format:
+            return [new_pos_x, new_pos_y]
 
     def get_position_available(self, pos_x, pos_y, piece, fn):
         if not piece.has_crown():
             return self.get_position_available_piece_not_queen(
                 pos_x,
                 pos_y,
-                piece
+                piece,
+                fn
             )
 
         if not self.check_position_is_valid(pos_x, pos_y):
@@ -183,4 +203,3 @@ class Board:
             and pos_y < 8 \
             and pos_x > -1 \
             and pos_x < 8
-
